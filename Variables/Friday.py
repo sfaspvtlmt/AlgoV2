680a32c33ce9e353d830b156adcace49e2a73d67
arushsarna <92302406+arushsarna@users.noreply.github.com>
2023-05-05 01:07:35 +0530

    .

--- Variables/Friday.py
+++ Variables/Friday.py
@@ -5,225 +5,225 @@ MyAccount = {
     "Name": "My Account",
     "Index": "FinNifty",
     "Strike": 0,
-    "HedgeStrike": 250,
+    "HedgeStrike": 400,
     "StopLoss": 35,
-    "HedgeQty": 760,
-    "Qty": 600,
-    "Hour": 9,
-    "Min": 44,
+    "HedgeQty": 520,
+    "Qty": 440,
+    "Hour": 10,
+    "Min": 29,
     "Seconds": 58,
     "Type": "Intraday",
-    "SLType": "TrailToCost"
+    "SLType": "Trailing"
 }
 
 MyAccount2 = {
 
     "Name": "My Account 2",
     "Index": "FinNifty",
-    "Strike": 0,
-    "HedgeStrike": 250,
-    "StopLoss": 35,
-    "HedgeQty": 600,
-    "Qty": 440,
-    "Hour": 10,
-    "Min": 29,
+    "Strike": 50,
+    "HedgeStrike": 400,
+    "StopLoss": 30,
+    "HedgeQty": 480,
+    "Qty": 360,
+    "Hour": 11,
+    "Min": 44,
     "Seconds": 58,
     "Type": "Intraday",
-    "SLType": "TrailToCost"
+    "SLType": "Trailing"
 }
 Riyaaz1 = {
 
     "Name": "Riyaaz 1",
     "Index": "FinNifty",
     "Strike": 0,
-    "HedgeStrike": 250,
+    "HedgeStrike": 400,
     "StopLoss": 35,
-    "HedgeQty": 4680,
-    "Qty": 3560,
-    "Hour": 9,
-    "Min": 44,
+    "HedgeQty": 3440,
+    "Qty": 2480,
+    "Hour": 10,
+    "Min": 29,
     "Seconds": 58,
     "Type": "Intraday",
-    "SLType": "TrailToCost"
+    "SLType": "Trailing"
 }
 Riyaaz2 = {
 
     "Name": "Riyaaz 2",
     "Index": "FinNifty",
-    "Strike": 0,
-    "HedgeStrike": 250,
-    "StopLoss": 35,
-    "HedgeQty": 3840,
-    "Qty": 2920,
-    "Hour": 10,
-    "Min": 29,
+    "Strike": 50,
+    "HedgeStrike": 400,
+    "StopLoss": 30,
+    "HedgeQty": 3400,
+    "Qty": 2440,
+    "Hour": 11,
+    "Min": 44,
     "Seconds": 58,
     "Type": "Intraday",
-    "SLType": "TrailToCost"
+    "SLType": "Trailing"
 }
 Riyaaz3 = {
 
     "Name": "Riyaaz 3",
     "Index": "FinNifty",
-    "Strike": 0,
-    "HedgeStrike": 250,
+    "Strike": 50,
+    "HedgeStrike": 400,
     "StopLoss": 35,
-    "HedgeQty": 1640,
-    "Qty": 1240,
-    "Hour": 11,
-    "Min": 29,
+    "HedgeQty": 1460,
+    "Qty": 1060,
+    "Hour": 9,
+    "Min": 44,
     "Seconds": 58,
     "Type": "Intraday",
-    "SLType": "TrailToCost"
+    "SLType": "Trailing"
 }
 Vijet1 = {
 
     "Name": "Vijet 1",
     "Index": "FinNifty",
     "Strike": 0,
-    "HedgeStrike": 250,
+    "HedgeStrike": 400,
     "StopLoss": 35,
-    "HedgeQty": 2720,
-    "Qty": 2080,
-    "Hour": 9,
-    "Min": 44,
+    "HedgeQty": 2240,
+    "Qty": 1600,
+    "Hour": 10,
+    "Min": 29,
     "Seconds": 58,
     "Type": "Intraday",
-    "SLType": "TrailToCost"
+    "SLType": "Trailing"
 }
 Vijet2 = {
 
     "Name": "Vijet 2",
     "Index": "FinNifty",
-    "Strike": 0,
-    "HedgeStrike": 250,
-    "StopLoss": 35,
-    "HedgeQty": 2720,
-    "Qty": 2080,
-    "Hour": 10,
-    "Min": 29,
+    "Strike": 50,
+    "HedgeStrike": 400,
+    "StopLoss": 30,
+    "HedgeQty": 2400,
+    "Qty": 1720,
+    "Hour": 11,
+    "Min": 44,
     "Seconds": 58,
     "Type": "Intraday",
-    "SLType": "TrailToCost"
+    "SLType": "Trailing"
 }
 Parag1 = {
 
     "Name": "Parag 1",
     "Index": "FinNifty",
     "Strike": 0,
-    "HedgeStrike": 250,
+    "HedgeStrike": 400,
     "StopLoss": 35,
-    "HedgeQty": 1600,
-    "Qty": 1120,
-    "Hour": 9,
-    "Min": 44,
+    "HedgeQty": 1060,
+    "Qty": 780,
+    "Hour": 10,
+    "Min": 29,
     "Seconds": 58,
     "Type": "Intraday",
-    "SLType": "TrailToCost"
+    "SLType": "Trailing"
 }
 Parag2 = {
 
     "Name": "Parag 2",
     "Index": "FinNifty",
-    "Strike": 0,
-    "HedgeStrike": 250,
-    "StopLoss": 35,
-    "HedgeQty": 1600,
-    "Qty": 1120,
-    "Hour": 10,
-    "Min": 29,
+    "Strike": 50,
+    "HedgeStrike": 400,
+    "StopLoss": 30,
+    "HedgeQty": 1100,
+    "Qty": 820,
+    "Hour": 11,
+    "Min": 44,
     "Seconds": 58,
     "Type": "Intraday",
-    "SLType": "TrailToCost"
+    "SLType": "Trailing"
 }
 Rajshekhar1 = {
 
     "Name": "Rajshekhar 1",
     "Index": "FinNifty",
     "Strike": 0,
-    "HedgeStrike": 250,
+    "HedgeStrike": 400,
     "StopLoss": 35,
-    "HedgeQty": 2720,
-    "Qty": 2080,
-    "Hour": 9,
-    "Min": 44,
+    "HedgeQty": 2000,
+    "Qty": 1480,
+    "Hour": 10,
+    "Min": 29,
     "Seconds": 58,
     "Type": "Intraday",
-    "SLType": "TrailToCost"
+    "SLType": "Trailing"
 }
 Rajshekhar2 = {
 
     "Name": "Rajshekhar 2",
     "Index": "FinNifty",
-    "Strike": 0,
-    "HedgeStrike": 250,
-    "StopLoss": 35,
-    "HedgeQty": 2720,
-    "Qty": 2080,
-    "Hour": 10,
-    "Min": 29,
+    "Strike": 50,
+    "HedgeStrike": 400,
+    "StopLoss": 30,
+    "HedgeQty": 2280,
+    "Qty": 1640,
+    "Hour": 11,
+    "Min": 44,
     "Seconds": 58,
     "Type": "Intraday",
-    "SLType": "TrailToCost"
+    "SLType": "Trailing"
 }
 Sumit1 = {
 
     "Name": "Sumit 1",
     "Index": "FinNifty",
     "Strike": 0,
-    "HedgeStrike": 250,
+    "HedgeStrike": 400,
     "StopLoss": 35,
-    "HedgeQty": 680,
-    "Qty": 520,
-    "Hour": 9,
-    "Min": 44,
+    "HedgeQty": 400,
+    "Qty": 320,
+    "Hour": 10,
+    "Min": 29,
     "Seconds": 58,
     "Type": "Intraday",
-    "SLType": "TrailToCost"
+    "SLType": "Trailing"
 }
 Sumit2 = {
 
     "Name": "Sumit 2",
     "Index": "FinNifty",
-    "Strike": 0,
-    "HedgeStrike": 250,
-    "StopLoss": 35,
-    "HedgeQty": 680,
-    "Qty": 520,
-    "Hour": 10,
-    "Min": 29,
+    "Strike": 50,
+    "HedgeStrike": 400,
+    "StopLoss": 30,
+    "HedgeQty": 480,
+    "Qty": 360,
+    "Hour": 11,
+    "Min": 44,
     "Seconds": 58,
     "Type": "Intraday",
-    "SLType": "TrailToCost"
+    "SLType": "Trailing"
 }
 
 Dilip1 = {
 
     "Name": "Dilip 1",
-    "Index": "Nifty",
+    "Index": "FinNifty",
     "Strike": 0,
-    "HedgeStrike": 300,
+    "HedgeStrike": 400,
     "StopLoss": 35,
-    "HedgeQty": 1150,
-    "Qty": 900,
-    "Hour": 9,
-    "Min": 44,
-    "Seconds": 58,
-    "Type": "Intraday",
-    "SLType": "TrailToCost"
-}
-Dilip2 = {
-
-    "Name": "Dilip 2",
-    "Index": "Nifty",
-    "Strike": 0,
-    "HedgeStrike": 300,
-    "StopLoss": 35,
-    "HedgeQty": 1150,
-    "Qty": 900,
+    "HedgeQty": 1060,
+    "Qty": 780,
     "Hour": 10,
     "Min": 29,
     "Seconds": 58,
     "Type": "Intraday",
-    "SLType": "TrailToCost"
+    "SLType": "Trailing"
+}
+Dilip2 = {
+
+    "Name": "Dilip 2",
+    "Index": "FinNifty",
+    "Strike": 50,
+    "HedgeStrike": 400,
+    "StopLoss": 30,
+    "HedgeQty": 1100,
+    "Qty": 820,
+    "Hour": 11,
+    "Min": 44,
+    "Seconds": 58,
+    "Type": "Intraday",
+    "SLType": "Trailing"
 }
