0f495fafd1d7b984616910336de43e3f71d771c0
arushsarna <136246885+sfaspvtlmt@users.noreply.github.com>
2023-06-12 04:17:07 +0530

    .

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def Main(Variables, api):
         print(Variables["Name"])
         print(now.hour, ":", now.minute, ":", now.second)
         time.sleep(1)
-        # if int(now.hour) == Variables["Hour"] and int(now.minute) == Variables["Min"] and int(now.second)>= Variables["Seconds"]:
-        Script.Search(api, Variables)
+        if int(now.hour) == Variables["Hour"] and int(now.minute) == Variables["Min"] and int(now.second)>= Variables["Seconds"]:
+         Script.Search(api, Variables)
 
 
